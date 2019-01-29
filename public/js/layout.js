7e3c149d271ae5a4e12d595e4608d9a9aa883dfa
Tyson G <tlgreen16@gmail.com>
2019-01-29 15:30:08 -0500

    selected css added

--- public/js/layout.js
+++ public/js/layout.js
@@ -7,23 +7,38 @@ const $grid = $('.box-container').isotope({
   percentPosition: true
 });
 
+function clear(){
+    $(".link-list li a").removeAttr("style");
+}
 
 $("#all").on("click",function(e){
   e.preventDefault();
+  clear()
+  $(".link-all").css("border-bottom","4px solid #F46F32");
   $grid.isotope({filter:"*"})
 })
 
 $("#profiles").on("click",function(e){
   e.preventDefault();
+  clear()
+  $(".link-profiles").css("border-bottom","4px solid #F46F32");
   $grid.isotope({filter:".profiles"})
 })
 
 $("#insights").on("click",function(e){
   e.preventDefault();
+  clear()
+  $(".link-insights").css("border-bottom","4px solid #F46F32");
   $grid.isotope({filter:".insights"})
 })
 
 $("#work").on("click",function(e){
   e.preventDefault();
+  clear()
+  $(".link-work").css("border-bottom","4px solid #F46F32");
   $grid.isotope({filter:".work"})
 })
+
+
+
+
