8a6796a9736b8ef0495418577b31718f5dee3e76
Tyson G <tlgreen16@gmail.com>
2019-02-02 01:03:24 -0500

    layout complete

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,43 +1,43 @@
 // initialize IIFE
 
 
-// const $grid = $('.box-container').isotope({
-//   itemSelector: '.box',
-//   layoutMode: 'fitRows',
-//   percentPosition: true
-// });
+const $grid = $('.box-container').isotope({
+  itemSelector: '.box',
+  layoutMode: 'fitRows',
+  percentPosition: true
+});
 
-// function clear(){
-//     $(".link-list li a").removeAttr("style");
-// }
+function clear(){
+    $(".link-list li a").removeAttr("style");
+}
 
-// $("#all").on("click",function(e){
-//   e.preventDefault();
-//   clear()
-//   $(".link-all").css("border-bottom","4px solid #F46F32");
-//   $grid.isotope({filter:"*"})
-// })
+$("#all").on("click",function(e){
+  e.preventDefault();
+  clear()
+  $(".link-all").css("border-bottom","4px solid #F46F32");
+  $grid.isotope({filter:"*"})
+})
 
-// $("#profiles").on("click",function(e){
-//   e.preventDefault();
-//   clear()
-//   $(".link-profiles").css("border-bottom","4px solid #F46F32");
-//   $grid.isotope({filter:".profiles"})
-// })
+$("#profiles").on("click",function(e){
+  e.preventDefault();
+  clear()
+  $(".link-profiles").css("border-bottom","4px solid #F46F32");
+  $grid.isotope({filter:".profiles"})
+})
 
-// $("#insights").on("click",function(e){
-//   e.preventDefault();
-//   clear()
-//   $(".link-insights").css("border-bottom","4px solid #F46F32");
-//   $grid.isotope({filter:".insights"})
-// })
+$("#insights").on("click",function(e){
+  e.preventDefault();
+  clear()
+  $(".link-insights").css("border-bottom","4px solid #F46F32");
+  $grid.isotope({filter:".insights"})
+})
 
-// $("#work").on("click",function(e){
-//   e.preventDefault();
-//   clear()
-//   $(".link-work").css("border-bottom","4px solid #F46F32");
-//   $grid.isotope({filter:".work"})
-// })
+$("#work").on("click",function(e){
+  e.preventDefault();
+  clear()
+  $(".link-work").css("border-bottom","4px solid #F46F32");
+  $grid.isotope({filter:".work"})
+})
 
 
 
